62b8867c151c7a16d44c9c5f23537384c0e8902c
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2023-04-20 17:59:48 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import RegisterForm from './components/RegisterForm';
 import MealsProvider from './components/DataProvider';
 import Multiple from './components/Form';
 /*
+// 
 function App() {
   return (
     <div className="App">
